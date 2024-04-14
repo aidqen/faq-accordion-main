e522779e514b9a00bec1cabad65e06258a646da2
aidqen <idanmarkin8@gmail.com>
2024-04-14 13:22:57 +0300

    Finished Accordion project

--- js/main.js
+++ js/main.js
@@ -1,14 +1,17 @@
 'use strict'
 
-function onInit() {
-  const elBtn = document.querySelectorAll('.btn')
-  const elAccContent = document.querySelector('.accordion-content')
+var elAccordion 
 
-  elBtn.forEach( btn => btn.addEventListener('click', () => {
-    btn.classList.toggle('active')
-    if (btn.classList.contains('active')) {
-        btn.src = '/assets/images/icon-minus.svg'
-    } else btn.src = '/assets/images/icon-plus.svg'
-    console.log(btn.nextElementSibling);
-  }))
+function onInit() {
+  elAccordion = document.querySelectorAll('.content-box')
+  // for (var i = 0 ; i < elAccordion.length ; i++) {
+  //   elAccordion[i].addEventListener('click', () => {
+  //     this.classList.toggle('active')
+  //   })
+  // }
+  elAccordion.forEach(element => {
+    element.addEventListener('click', () => {
+      element.classList.toggle('active')
+    })
+  })
 }
